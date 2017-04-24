e8e9d6d36f6e91562da603cbff9fbbe49df833c7
silvia <sylviadigiorgio@gmail.com>
2017-04-24 15:17:25 +0200

    with argparse

--- bin/GRADitude.py
+++ bin/GRADitude.py
@@ -1,5 +1,5 @@
 import argparse
-#from graditudelib import normalize
+from graditudelib import normalize
 
 
 def main():
@@ -11,14 +11,37 @@ def main():
     counting_table_parser.add_argument("--feature_counting_table", help='parse the counting table')
     counting_table_parser.set_defaults(func=table_parser)
 
+    spike_in_counting_table = subparsers.add_parser('spiked_in_counting_table')
+    spike_in_counting_table.add_argument("--feature_spiked_in_counting_table")
+    spike_in_counting_table.set_defaults(func=spiked_in_table_parser)
+
+    minimal_counting_feature = subparsers.add_parser('minimal_counting_feature')
+    minimal_counting_feature.add_argument("--feature_minimal_counting_table", help='parse the minimal counting table')
+    minimal_counting_feature.set_defaults(func=minimal_table_parser)
+
+    minimal_counting_spiked_in = subparsers.add_parser('minimal_counting_spiked_in')
+    minimal_counting_spiked_in.add_argument("--feature_minimal_counting_spiked_in_table")
+    minimal_counting_spiked_in.set_defaults(func=minimal_spike_in_table_parser)
+
     args = parser.parse_args()
     if "func" in dir(args):
         args.func(args)
 
 
 def table_parser(args):
-    # print(args)
-    # print(args.feature_counting_table)
-    #normalize.normalize(args)
+    normalize.normalize(args)
+
+
+def minimal_table_parser(args):
+    normalize.normalize(args)
+
+
+def spiked_in_table_parser(args):
+    normalize.normalize(args)
+
+
+def minimal_spike_in_table_parser(args):
+    normalize.normalize(args)
+
 
 main()
--- graditudelib/normalize.py
+++ graditudelib/normalize.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def normalize(args):
     print(args.feature_counting_table)
-    table = pd.read_table(args.feature_counting_table)
-    return table
-
-
+    feature_counting_table = pd.read_table(args.feature_counting_table)
+    feature_spiked_in_counting_table = pd.read_table(args.feature_spiked_in_counting_table)
+    feature_minimal_counting_feature_table = pd.read_table(args.feature_minimal_counting_feature_table)
+    return feature_counting_table, feature_spiked_in_counting_table, feature_minimal_counting_feature_table
