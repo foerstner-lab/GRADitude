37c7cbbe6b43232729b469dc58ac95b9989f0d28
silvia <sylviadigiorgio@gmail.com>
2017-05-07 18:28:33 +0200

    with size factor

--- graditudelib/normalize.py
+++ graditudelib/normalize.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 from scipy.stats.mstats import gmean
+import numpy as np
+
 
 
 # def normalize(args):
@@ -16,7 +18,9 @@ def main():
 
     g_mean = geometric_mean_by_row(filtered_table)
 
-    divide_rows_by_geometric_mean(filtered_table, g_mean)
+    normalized_table = divide_rows_by_geometric_mean(filtered_table, g_mean)
+
+    get_medians(normalized_table)
 
 
 def create_a_new_table(alignment_table):
@@ -36,13 +40,25 @@ def geometric_mean_by_row(table):
 
 
 def divide_rows_by_geometric_mean(table, g_mean):
+    rows = []
     for index, row in table.reset_index(drop=True).iterrows():
         if g_mean[index] != 0:
-            new_row = row.multiply(1 / g_mean[index])
-        else:
-            print("g_mean[index] == 0")
+            rows.append(row.multiply(1 / g_mean[index]))
+        # else:
+            # print("g_mean[index] == 0")
+            # rows.append(row.multiply(g_mean[index]))
 
-        # print(new_row)
+    return pd.DataFrame(rows)
 
+
+def get_medians(table):
+    for col_name in table.columns:
+        index = table.columns.get_loc(col_name)
+        if index == 0:
+            continue
+
+        column = table.iloc[0:, index]
+        median_value = np.median(column)
+        print(median_value)
 
 main()
