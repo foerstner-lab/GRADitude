1d3eb5ec267bdab57bedf818330fa12c72051888
silvia <sylviadigiorgio@gmail.com>
2017-05-07 12:45:10 +0200

    with filtering

--- bin/GRADitude.py
+++ bin/GRADitude.py
@@ -7,9 +7,17 @@ def main():
     parser.add_argument("--version", "-v", action="store_true", help="show version")
     subparsers = parser.add_subparsers(help="commands")
 
-    counting_table_parser = subparsers.add_parser("counting-table")
+    normalize_parser = subparsers.add_parser("normalize")
+    normalize_parser.set_defaults(func=normalize_table)
+    normalize_parser.add_argument("--feature_counting_table", required=True)
+    normalize_parser.add_argument("--spike_in_counting_table", required=True)
+    normalize_parser.add_argument("--minimal_counting_feature", type=int)
+
+    """    
+    counting_table_parser = subparsers.add_parser("--spike-in-counting-table", "-s")
+    
     counting_table_parser.add_argument("--feature_counting_table", help='parse the counting table')
-    counting_table_parser.set_defaults(func=table_parser)
+    
 
     spike_in_counting_table = subparsers.add_parser('spiked_in_counting_table')
     spike_in_counting_table.add_argument("--feature_spiked_in_counting_table")
@@ -22,12 +30,18 @@ def main():
     minimal_counting_spiked_in = subparsers.add_parser('minimal_counting_spiked_in')
     minimal_counting_spiked_in.add_argument("--feature_minimal_counting_spiked_in_table")
     minimal_counting_spiked_in.set_defaults(func=minimal_spike_in_table_parser)
-
+    """
     args = parser.parse_args()
     if "func" in dir(args):
         args.func(args)
 
 
+def normalize_table(args):
+    normalize.normalize(args.feature_counting_table)
+    normalize.normalize(args.spike_in_counting_table)
+    normalize.normalize(args.minimal_counting_feature)
+
+"""
 def table_parser(args):
     normalize.normalize(args)
 
@@ -42,6 +56,6 @@ def spiked_in_table_parser(args):
 
 def minimal_spike_in_table_parser(args):
     normalize.normalize(args)
-
+"""
 
 main()
--- graditudelib/normalize.py
+++ graditudelib/normalize.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 
 import pandas as pd
+import os
+import csv
+
+# def normalize(args):
+#     feature_counting_table = pd.read_table(args.feature_counting_table)
+#     return feature_counting_table
 
 
-def normalize(args):
-    print(args.feature_counting_table)
-    feature_counting_table = pd.read_table(args.feature_counting_table)
-    feature_spiked_in_counting_table = pd.read_table(args.feature_spiked_in_counting_table)
-    feature_minimal_counting_feature_table = pd.read_table(args.feature_minimal_counting_feature_table)
-    return feature_counting_table, feature_spiked_in_counting_table, feature_minimal_counting_feature_table
+def main():
+    create_a_new_table('read_alignment_stats.csv')
+
+
+def create_a_new_table(files):
+    series = []
+    alignment_table = pd.read_table(files)
+    for index, row in alignment_table.iterrows():
+        selection = row['Libraries']
+        if selection.startswith('ERCC') & ('No. of aligned reads' in selection):
+            series.append(row)
+    filtered_table = pd.DataFrame(series)
+    filtered_table.to_csv('filtered_alignment_stats.csv')
+
+main()
