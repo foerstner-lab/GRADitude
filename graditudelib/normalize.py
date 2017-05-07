ca1563c35c340a757aefaa50a748ec70b2ac6925
silvia <sylviadigiorgio@gmail.com>
2017-05-07 13:31:23 +0200

    with geometric_mean

--- graditudelib/normalize.py
+++ graditudelib/normalize.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 import pandas as pd
-import os
-import csv
+from scipy.stats.mstats import gmean
 
 # def normalize(args):
 #     feature_counting_table = pd.read_table(args.feature_counting_table)
@@ -11,6 +10,7 @@ import csv
 
 def main():
     create_a_new_table('read_alignment_stats.csv')
+    geometric_mean('filtered_alignment_stats.csv')
 
 
 def create_a_new_table(files):
@@ -21,6 +21,13 @@ def create_a_new_table(files):
         if selection.startswith('ERCC') & ('No. of aligned reads' in selection):
             series.append(row)
     filtered_table = pd.DataFrame(series)
-    filtered_table.to_csv('filtered_alignment_stats.csv')
+    filtered_table.to_csv('filtered_alignment_stats.csv', index =None)
+
+
+def geometric_mean(table):
+    table_new = pd.read_table(table, sep=",")
+    table_new['g_mean'] = gmean(table_new.iloc[0:, 1:], axis=1)
+    return table_new.to_csv('alignment_stats_with_g_mean.csv', index=None)
+
 
 main()
