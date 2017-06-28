f3f86b4305fb4baeb78f6836ee47b3d02b7684e7
Konrad Förstner <konrad@foerstner.org>
2017-06-28 16:57:03 +0200

    Cleanup and renaming

--- graditudelib/size_factor.py
+++ graditudelib/size_factor.py
@@ -6,40 +6,16 @@ import os
 
 
 def main(params):
-    read_alignment_stats = params.read_alignment_stats
-    alignment_table = pd.read_table(read_alignment_stats)
-    create_a_new_table(alignment_table, 'filtered_alignment_stats.csv')
-    csv_file = read_filtered_alignment_stats(config.OUTPUT_DIR + 'filtered_alignment_stats.csv')
-    g_mean = geometric_mean_by_row(csv_file)
+    reference_read_counts = pd.read_table(params.reference_read_count_file)
+    g_mean = geometric_mean_by_row(reference_read_counts)
     normalized_table = divide_rows_by_geometric_mean(csv_file, g_mean)
     value = get_medians(normalized_table)
     print_to_csv(value, 'size_factor_ERCC_to_normalize_tables_without_pellet.csv')
     sf_table = read_table_size_factor(config.SIZE_FACTORS_DIR +
                                       'size_factor_ERCC_to_normalize_tables_without_pellet.csv')
-    q_table = read_table_quanti(params.gene_wise_quantification)
-    table = new_table_(sf_table, q_table)
+    read_counts = read_table_quanti(params.read_count_file)
+    table = new_table_(sf_table, read_counts)
     table.to_csv(config.OUTPUT_DIR + 'normalized_table_ERCC_without_pellet.csv', index=0)
-
-
-def create_a_new_table(alignment_table, output_file):
-    if not os.path.exists(config.OUTPUT_DIR):
-        os.makedirs(config.OUTPUT_DIR)
-    series = []
-    for index, row in alignment_table.iterrows():
-        selection = row['Libraries']
-        if selection.startswith('ERCC') & ('No. of aligned reads' in selection):
-            series.append(row)
-
-    filtered_table = pd.DataFrame(series)
-    filtered_table.to_csv(config.OUTPUT_DIR + output_file, index=None)
-    return filtered_table
-
-
-def read_filtered_alignment_stats(file):
-    table = pd.read_csv(file, sep=',')
-    table.drop(table.columns[[1, -1]], axis=1, inplace=True)  # without pellet
-    # table.drop(table.columns[[1]], axis=1, inplace=True) #with_pellet
-    return table
 
 
 def geometric_mean_by_row(table):
