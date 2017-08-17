d4ac66fd0800bb57063f48bcd8135eab8d888d41
silvia <sylviadigiorgio@gmail.com>
2017-08-17 10:24:40 +0200

    K-means with normalization

--- graditudelib/k_means.py
+++ graditudelib/k_means.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 
@@ -5,13 +6,14 @@ from sklearn.cluster import KMeans
 def generate_k_means_clustering(feature_count_table,
                                 feature_count_start_column,
                                 number_of_clusters,
-                                output_file):
+                                output_file, scaling_method, pseudo_count):
     feature_count_table_df = pd.read_table(feature_count_table)
     value_matrix = _extract_value_matrix(feature_count_table_df,
                                          feature_count_start_column)
+    new_table = normalize_values(value_matrix, scaling_method, pseudo_count)
     attribute_matrix = _extract_attributes(feature_count_table_df,
                                            feature_count_start_column)
-    clustering_table = k_means_clustering(value_matrix, number_of_clusters)
+    clustering_table = k_means_clustering(new_table, number_of_clusters)
     pd.concat([attribute_matrix, clustering_table],
               axis=1).to_csv(output_file, sep='\t')
 
@@ -34,3 +36,30 @@ def k_means_clustering(values_matrix, number_of_clusters):
     pd.DataFrame(data=labels, columns=['cluster'])
     values_matrix["Cluster_label"] = pd.Series(k_means.labels_).astype(int)
     return values_matrix
+
+
+def normalize_values(read_counting_table, scaling_method, pseudo_count):
+    pseudo_count = 1.0
+    read_countings_values_only = read_counting_table[list(filter(
+        lambda col: col.startswith("Grad_47"), read_counting_table.columns))]
+    if scaling_method == "no_normalization":
+        normalized_values = read_countings_values_only
+    elif scaling_method == "log2":
+        normalized_values = read_countings_values_only.applymap(
+            lambda val: val + pseudo_count).applymap(np.log2)
+    elif scaling_method == "log10":
+        normalized_values = read_countings_values_only.applymap(
+            lambda val: val + pseudo_count).applymap(np.log10)
+    elif scaling_method == "normalized_to_max":
+        row_max_values = read_countings_values_only.max(axis=1)
+        normalized_values = read_countings_values_only.divide(
+            row_max_values, axis=0)
+    elif scaling_method == "normalized_to_range":
+        row_max_values = read_countings_values_only.max(axis=1)
+        row_min_values = read_countings_values_only.min(axis=1)
+        normalized_values = read_countings_values_only.subtract(
+            row_min_values, axis=0).divide(row_max_values, axis=0)
+    else:
+        print("Normalization method not known")
+    return normalized_values
+
