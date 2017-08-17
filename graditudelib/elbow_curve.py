c658c3b6acb1db7b6ebe3f840c9c882d390432bd
silvia <sylviadigiorgio@gmail.com>
2017-08-17 16:32:48 +0200

    Formatting elbow method

--- graditudelib/elbow_curve.py
+++ graditudelib/elbow_curve.py
@@ -1,12 +1,22 @@
+import numpy as np
+import pandas as pd
 from scipy.spatial.distance import cdist, pdist
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-import numpy as np
 
 
-def k_means_clustering_elbow(value_matrix,
-                             min_number_of_clusters,
-                             max_number_of_clusters, output_plot):
+def k_means_clustering_elbow(feature_count_table, feature_count_start_column, min_number_of_clusters,
+                             max_number_of_clusters):
+
+    feature_count_table_df = pd.read_table(feature_count_table)
+    value_matrix = _extract_value_matrix(feature_count_table_df,
+                                         feature_count_start_column)
+    elbow_methods(value_matrix, min_number_of_clusters, max_number_of_clusters)
+    elbow_methods1(value_matrix, min_number_of_clusters, max_number_of_clusters)
+
+
+def elbow_methods(value_matrix, min_number_of_clusters,
+                  max_number_of_clusters):
     range_k = range(int(min_number_of_clusters), int(max_number_of_clusters))
     k_m = [KMeans(n_clusters=k).fit(value_matrix) for k in range_k]
     centroids = [k.cluster_centers_ for k in k_m]
@@ -22,7 +32,25 @@ def k_means_clustering_elbow(value_matrix,
     plt.xlabel('Number of clusters')
     plt.ylabel('Percentage of variance explained')
     plt.title('Elbow for KMeans clustering')
-    plt.savefig(output_plot)
+    plt.savefig('elbow_for_kmeans_clustering2.pdf', format='pdf')
+
+
+def elbow_methods1(value_matrix, min_number_of_clusters,
+                   max_number_of_clusters):
+    range_k = range(int(min_number_of_clusters), int(max_number_of_clusters))
+    k_m = [KMeans(n_clusters=k).fit(value_matrix) for k in range_k]
+    centroids = [k.cluster_centers_ for k in k_m]
+    d_k = [cdist(value_matrix, cent, 'euclidean') for cent in centroids]
+    dist = [np.min(D, axis=1) for D in d_k]
+    avg_within_sum_of_squares = [sum(d)/value_matrix.shape[0] for d in dist]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(range_k, avg_within_sum_of_squares, 'b*-')
+    plt.grid(True)
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Average within-cluster sum of squares')
+    plt.title('Elbow for KMeans clustering')
+    plt.savefig('elbow_for_kmeans_clustering.pdf', format='pdf')
 
 
 def _extract_value_matrix(feature_count_table_df,
--- tests/test_input.py
+++ tests/test_input.py
@@ -1,6 +1,9 @@
 from graditudelib import normalize
 from graditudelib import visualizing_kinetics
 from graditudelib import k_means
+from graditudelib import elbow_curve
+from graditudelib import silhouette
+from graditudelib import tSNE
 
 
 def test_run_normalize():
@@ -27,11 +30,38 @@ def test_run_k_means_clustering():
         "../data/gene_wise_quantifications_combined_extended_test.csv",
         12,
         7,
-        'normalized_by_max_value_with_clusters.csv',
+        'normalized_by_log10_with_clusters.csv',
         'log10',
         1)
 
 
+def test_run_elbow_method():
+    elbow_curve.k_means_clustering_elbow(
+        "../data/gene_wise_quantifications_combined_extended_test.csv",
+        13,
+        2,
+        10)
+
+
+def test_run_silhouette_analysis():
+    silhouette.silhouette_analysis(
+        "../data/gene_wise_quantifications_combined_extended_test.csv",
+        13,
+        2,
+        10)
+
+
+def test_run_t_sne_analysis():
+    tSNE.t_sne_analysis("../tests/normalized_by_log10_with_clusters.csv",
+                        13,
+                        1,
+                        'log10')
+
+
+
 #test_run_normalize()
 #test_run_visualizing_kinetics()
-test_run_k_means_clustering()
+#test_run_k_means_clustering()
+#test_run_elbow_method()
+#test_run_silhouette_analysis()
+test_run_t_sne_analysis()
