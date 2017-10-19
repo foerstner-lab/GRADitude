118d733b23d5b38a84dfc9ff937f3a843dfd23d6
silvia <sylviadigiorgio@gmail.com>
2017-10-19 15:09:07 +0200

    corrections

--- graditudelib/robust_regression.py
+++ graditudelib/robust_regression.py
@@ -8,50 +8,26 @@ from sklearn.linear_model import RANSACRegressor
 
 def robust_regression(
         ref_feature_count_table, concentration_table, number_of_outliers, output_file):
-    ref_feature_count_table_df = read_table(ref_feature_count_table)
-    concentration_table_df = read_table_concentration(concentration_table)
-    gradient_file_combined = merging_dict(ref_feature_count_table_df, concentration_table_df)
+    ref_feature_count_table_df = pd.read_table(ref_feature_count_table, sep=',')
+    concentration_table_df = pd.read_table(concentration_table)
+    gradient_file_combined = merging_df(ref_feature_count_table_df, concentration_table_df)
     read_grad_value, read_concentration_value, gradient_file = \
         modify_input(gradient_file_combined)
-    common_ercc_df = regression(read_grad_value, read_concentration_value, gradient_file,
-                                number_of_outliers)
+    common_ercc_df, x_values, y_values = regression(read_grad_value, read_concentration_value, gradient_file,
+                                                    number_of_outliers)
+    regression_plot(read_grad_value, x_values, y_values, gradient_file, number_of_outliers)
     df_common_ercc = common_ercc(common_ercc_df)
     new_ref_table(df_common_ercc, ref_feature_count_table_df, output_file)
 
 
-def read_table(ref_feature_count_table):
-    ref_feature_count_table_dict = {}
-    ref_feature_count_table_df = pd.read_table(ref_feature_count_table, sep=',')
-    for index, row in ref_feature_count_table_df.iterrows():
-        key = row[0][:row[0].index(' ')]
-        ref_feature_count_table_dict[key] = row
-    return ref_feature_count_table_dict
-
-
-def read_table_concentration(concentration_table):
-    new_dict_for_conc_table = {}
-    concentration_table_df = pd.read_table(concentration_table)
-    for index, row in concentration_table_df.iterrows():
-        new_dict_for_conc_table[row[1]] = row
-    return new_dict_for_conc_table
-
-
-def merging_dict(ref_feature_count_table_dict, new_dict_for_conc_table):
-    combined_dict = {}
-    concentration_list = []
-    for key in ref_feature_count_table_dict:
-        reads = ref_feature_count_table_dict[key][1:]
-        concentration = new_dict_for_conc_table[key][4]
-        for gradient, val in reads.iteritems():
-            if gradient not in combined_dict:
-                combined_dict[gradient] = {}
-            combined_dict[gradient][key] = val
-        concentration_list.append(concentration)
-    concentration_serie = pd.Series(concentration_list)
-    combined_df = pd.DataFrame.from_dict(combined_dict)
-    combined_df.loc[:, 'Concentration'] = concentration_serie.values
-    combined_df.reset_index(inplace=True)
-    return combined_df
+def merging_df(ref_feature_count_table_df, concentration_table_df):
+    concentration_table_df_sort = concentration_table_df.sort_values(['ERCC_ID'])
+    concentration_column = concentration_table_df_sort.iloc[:, 4]
+    concentration_column_reset = concentration_column.reset_index()
+    mixed_df = pd.concat([ref_feature_count_table_df, concentration_column_reset], axis=1)
+    mixed_df.drop('index', axis=1, inplace=True)
+    mixed_df.columns.values[-1] = 'Concentration'
+    return mixed_df
 
 
 def modify_input(gradient_file):
@@ -73,6 +49,23 @@ def regression(read_grad_value, read_concentration_value, gradient_file,
         concentration_log10[concentration_log10 == -inf] = 0
         x_values = read_grad_value_log10.reshape(len(key), 1)
         y_values = concentration_log10.reshape(len(key), 1)
+        clt_ransac = linear_model.RANSACRegressor(linear_model.LinearRegression())
+        clt_ransac.fit(x_values, y_values)
+        robust_estimator = RANSACRegressor(random_state=0)
+        robust_estimator.fit(x_values, y_values)
+        distance_pred = robust_estimator.predict(x_values)
+        mean_squared_error = (y_values - distance_pred) ** 2
+        index = np.argsort(mean_squared_error.ravel())
+        outliers_keys = key[index[-number_of_outliers:]]
+        table_with_outliers[gradient] = outliers_keys
+    outliers_df = pd.DataFrame(table_with_outliers)
+    return outliers_df, x_values, y_values
+
+
+def regression_plot(read_grad_value, x_values, y_values, gradient_file,
+                    number_of_outliers):
+    key = gradient_file.ix[:, 0].ravel()
+    for gradient in read_grad_value:
         clt_ransac = linear_model.RANSACRegressor(linear_model.LinearRegression())
         clt_ransac = clt_ransac.fit(x_values, y_values)
         fig, axes = plt.subplots(ncols=2)
@@ -99,11 +92,7 @@ def regression(read_grad_value, read_concentration_value, gradient_file,
                   str(clt_ransac.estimator_.coef_),
                   fontsize=10)
         plt.savefig(gradient)
-        plt.close()
-        outliers_keys = key[index[-number_of_outliers:]]
-        table_with_outliers[gradient] = outliers_keys
-    outliers_df = pd.DataFrame(table_with_outliers)
-    return outliers_df
+    plt.close()
 
 
 def common_ercc(csv_file):
@@ -128,9 +117,10 @@ def new_ref_table(csv_common_ercc, ref_table, output_file):
     new_csv_common_ercc_df.columns = ['keys', 'values']
     selected_df = new_csv_common_ercc_df[~(new_csv_common_ercc_df['values'] >= 18)]
     selected_df.reset_index(drop=True, inplace=True)
-    ref_table_df = pd.DataFrame.from_dict(ref_table, orient='index')
+    new_csv_common_ercc_df.set_index('keys', inplace=True)
+    ref_table.set_index('Libraries', inplace=True)
     my_keys = selected_df['keys'].tolist()
-    for index, row in ref_table_df.iterrows():
+    for index, row in ref_table.iterrows():
         if index in my_keys:
             ercc_table_final.append(row)
     df_with_selected_ercc = pd.DataFrame(ercc_table_final)
