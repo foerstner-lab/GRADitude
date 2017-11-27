da1854d57a8f6bce3e0c380f203c96a819983a2f
silvia <sylviadigiorgio@gmail.com>
2017-11-27 13:46:49 +0100

    Dropping columns

--- graditudelib/modify_input.py
+++ graditudelib/modify_input.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 
 def filtering_input(feature_count_table, column_to_drop, ref_feature_count_table, min_row_sum,
-                    filtered_feature_count_table, filtered_ref_feature_count_table):
+                    filtered_feature_count_table, filtered_ref_feature_count_table, dropping_lisate):
     feature_count_table_df = pd.read_table(feature_count_table, sep='\t')
     table_filtered_gene_quanti = create_gene_quanti_table_modified(feature_count_table_df,
-                                                                   column_to_drop)
+                                                                   column_to_drop, dropping_lisate)
     ref_feature_count_table_df = pd.read_table(ref_feature_count_table, sep='\t')
-    table_filtered_ercc = create_a_new_table_filtered_ercc(ref_feature_count_table_df)
+    table_filtered_ercc = create_a_new_table_filtered_ercc(ref_feature_count_table_df, dropping_lisate)
     colum_with_libraries = _extract_gene_matrix(table_filtered_ercc)
     table_filtered_min_row_sum = min_row_sum_ercc_table(table_filtered_ercc, colum_with_libraries,
                                                         min_row_sum)
@@ -21,7 +21,7 @@ def _extract_gene_matrix(feature_count_table_df):
     return gene_column
 
 
-def create_a_new_table_filtered_ercc(alignment_table):
+def create_a_new_table_filtered_ercc(alignment_table, dropping_lisate):
     series = []
     for index, row in alignment_table.iterrows():
         selection = row['Libraries']
@@ -29,7 +29,7 @@ def create_a_new_table_filtered_ercc(alignment_table):
             series.append(row)
 
     filtered_table = pd.DataFrame(series)
-    filtered_table.drop('Grad_47_Fraction_00L_Rep_3', axis=1, inplace=True)
+    filtered_table.drop(dropping_lisate, axis=1, inplace=True)
     return filtered_table
 
 
@@ -50,12 +50,12 @@ def min_row_sum_ercc_table(table_filtered_ercc, colum_with_libraries, min_row_su
     return df_with_min_row_samples
 
 
-def create_gene_quanti_table_modified(gene_quanti_table, column_to_drop):
+def create_gene_quanti_table_modified(gene_quanti_table, column_to_drop, dropping_lisate):
     cols = gene_quanti_table.columns.tolist()
     cols = cols[-1:] + cols[:-1]
     gene_quanti_table_with_gene_column_modified = gene_quanti_table[cols]
     gene_without_column = gene_quanti_table_with_gene_column_modified.drop(column_to_drop, axis=1)
-    gene_without_column.drop('Grad_47_Fraction_00L_Rep_3', axis=1, inplace=True)
+    gene_without_column.drop(dropping_lisate, axis=1, inplace=True)
     return gene_without_column
 
 
