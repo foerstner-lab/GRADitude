0522fb20d72d294cd82473ab7b74dd5a2d94d318
silvia <sylviadigiorgio@gmail.com>
2017-11-27 11:20:50 +0100

    Adding end_column as parameter

--- graditudelib/normalize.py
+++ graditudelib/normalize.py
@@ -5,30 +5,33 @@ import scipy.stats.mstats
 def normalized_count_table(
         feature_count_table,
         feature_count_start_column,
+        feature_count_end_column,
         ref_feature_count_table,
         ref_feature_count_start_column,
+        ref_feature_count_end_column,
         normalized_table,
         size_factor_table):
     ref_feature_count_table_df = pd.read_table(ref_feature_count_table, sep='\t')
     feature_count_table_df = pd.read_table(feature_count_table)
     size_factors = _calc_size_factors(ref_feature_count_table_df,
-                                      ref_feature_count_start_column)
+                                      ref_feature_count_start_column, ref_feature_count_end_column)
     normalized_table_df = _normalize_by_size_factor(feature_count_table_df,
-                                                    feature_count_start_column, size_factors)
+                                                    feature_count_start_column,
+                                                    feature_count_end_column, size_factors)
     normalized_table_df.to_csv(normalized_table, sep='\t', index=None)
     size_factors.to_csv(size_factor_table, sep='\t', header=['size_factors'])
 
 
 def _extract_value_matrix(feature_count_table_df,
-                          feature_count_start_column):
-    return feature_count_table_df.iloc[:, int(feature_count_start_column):]
+                          feature_count_start_column, feature_count_end_column):
+    return feature_count_table_df.iloc[:, int(feature_count_start_column):feature_count_end_column]
 
 
 def _calc_size_factors(ref_feature_count_table_df,
-                       ref_feature_count_start_column):
+                       ref_feature_count_start_column, ref_feature_count_end_column):
     # TODO: Documentation the calculation with formula
     counting_value_matrix = _extract_value_matrix(
-        ref_feature_count_table_df, ref_feature_count_start_column)
+        ref_feature_count_table_df, ref_feature_count_start_column, int(ref_feature_count_end_column))
     geometric_means = _geometric_means(counting_value_matrix)
     scaled_counting_value_matrix = _multiply_geometric_means_with_value_matrix(
         counting_value_matrix, geometric_means)
@@ -50,10 +53,11 @@ def _geometric_means(counting_value_matrix):
 
 
 def _normalize_by_size_factor(feature_counting_table_df,
-                              feature_count_start_column, size_factor):
+                              feature_count_start_column, feature_count_end_column, size_factor):
     df = feature_counting_table_df.copy()
-    value_columns = df.columns[feature_count_start_column:]
+    df_selected = df.iloc[:, :int(feature_count_end_column)]
+    value_columns = df_selected.columns[feature_count_start_column:]
     """Divide the gene quantification table for the size factor to normalize the data"""
-    df[value_columns] = df[value_columns].divide(list(size_factor), axis=1)
-    return df
+    df_selected[value_columns] = df_selected[value_columns].divide(list(size_factor), axis=1)
+    return df_selected
 
