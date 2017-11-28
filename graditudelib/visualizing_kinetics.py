352b1f494f6bead6a41e85300f02ae605b3df59f
silvia <sylviadigiorgio@gmail.com>
2017-11-28 18:06:01 +0100

    Fixing int problem

--- graditudelib/visualizing_kinetics.py
+++ graditudelib/visualizing_kinetics.py
@@ -11,7 +11,6 @@ def plot_kinetics(feature_count_table,
 
     gene_row = _extract_gene_row(feature_count_table_df, gene_name)
     value_gene_row = _extract_value_gene_row(gene_row, feature_count_start_column).values.T.tolist()
-    _plot_gene_png(value_gene_row, gene_name)
     _plot_gene_html(value_gene_row, gene_name)
 
 
@@ -32,18 +31,4 @@ def _plot_gene_html(counting_value_list, gene_name):
     y_axis = range(0, 21)
     plot_html.line(y_axis, counting_value_list)
     show(plot_html)
-
-
-def _plot_gene_png(counting_value_list, gene_name):
-    flattened = [val for sublist in counting_value_list for val in sublist]
-    flattened_series = pd.Series(flattened)
-    x_axis = range(1, 22)
-    interpolation = interpolate.interp1d(x_axis, flattened_series, kind="linear")
-    x_int = np.linspace(x_axis[0], x_axis[-1], 20)
-    y_int = interpolation(x_int)
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.plot(x_axis, flattened_series, color='red', label='Unsmoothed curve')
-    ax.plot(x_int, y_int, color="blue", label="Interpolated curve")
-    plt.show()
-    plt.savefig(gene_name + '.png')
 
