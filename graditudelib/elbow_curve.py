3bb02896ecc6e55e4025c737d160b860d657d287
silvia <sylviadigiorgio@gmail.com>
2018-10-31 10:18:04 +0100

    increasing the font

--- graditudelib/elbow_curve.py
+++ graditudelib/elbow_curve.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 def k_means_clustering_elbow(feature_count_table, feature_count_start_column, feature_count_end_column,
                              min_number_of_clusters, max_number_of_clusters, output_plots1, output_plot2):
-
     feature_count_table_df = pd.read_table(feature_count_table)
     value_matrix = _extract_value_matrix(feature_count_table_df,
                                          feature_count_start_column, feature_count_end_column)
@@ -28,11 +27,11 @@ def elbow_methods(value_matrix, min_number_of_clusters,
     bss = tss - w_css
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(range_k, bss/tss*100, 'b*-')
+    ax.plot(range_k, bss / tss * 100, 'b*-')
     plt.grid(True)
-    plt.xlabel('Number of clusters')
-    plt.ylabel('Percentage of variance explained')
-    plt.title('Elbow for KMeans clustering')
+    plt.xlabel('Number of clusters', fontsize=15)
+    plt.ylabel('Percentage of variance explained', fontsize=15)
+    plt.title('Elbow for KMeans clustering', fontsize=18)
     plt.savefig(output_plots1, format='pdf')
 
 
@@ -43,19 +42,17 @@ def elbow_methods1(value_matrix, min_number_of_clusters,
     centroids = [k.cluster_centers_ for k in k_m]
     d_k = [cdist(value_matrix, cent, 'euclidean') for cent in centroids]
     dist = [np.min(D, axis=1) for D in d_k]
-    avg_within_sum_of_squares = [sum(d)/value_matrix.shape[0] for d in dist]
+    avg_within_sum_of_squares = [sum(d) / value_matrix.shape[0] for d in dist]
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(range_k, avg_within_sum_of_squares, 'b*-')
     plt.grid(True)
-    plt.xlabel('Number of clusters')
-    plt.ylabel('Average within-cluster sum of squares')
-    plt.title('Elbow for KMeans clustering')
+    plt.xlabel('Number of clusters', fontsize=15)
+    plt.ylabel('Average within-cluster sum of squares', fontsize=15)
+    plt.title('Elbow for KMeans clustering', fontsize=18)
     plt.savefig(output_plot2, format='pdf')
 
 
 def _extract_value_matrix(feature_count_table_df,
                           feature_count_start_column, feature_count_end_column):
     return feature_count_table_df.iloc[:, int(feature_count_start_column):int(feature_count_end_column)]
-
-
