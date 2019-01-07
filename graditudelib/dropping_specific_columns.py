63f0ccb111b42ebb7cf1b1066664f631ec0925f4
silvia <sylviadigiorgio@gmail.com>
2019-01-07 17:22:35 +0100

    fix a name

--- graditudelib/dropping_specific_columns.py
+++ graditudelib/dropping_specific_columns.py
@@ -3,15 +3,16 @@ import pandas as pd
 
 def dropping_column(feature_count_table, column_to_drop, output_file):
     feature_count_table_df = pd.read_table(feature_count_table)
-    table_filtered_gene_quanti = create_gene_quanti_table_modified(feature_count_table_df,column_to_drop)
+    table_filtered_gene_quanti = create_gene_quanti_table_modified(feature_count_table_df,
+                                                                   column_to_drop)
     table_filtered_gene_quanti.to_csv(output_file, sep='\t', index=None)
 
 
-def create_gene_quanti_table_modified(gene_quanti_table, dropping_lisate):
+def create_gene_quanti_table_modified(gene_quanti_table, column_to_drop):
     cols = gene_quanti_table.columns.tolist()
-    cols = cols[-1:] + cols[:-1]
     gene_quanti_table_with_gene_column_modified = gene_quanti_table[cols]
-    gene_quanti_table_with_gene_column_modified.drop(dropping_lisate, axis=1, inplace=True)
+    gene_quanti_table_with_gene_column_modified.drop(column_to_drop, axis=1,
+                                                     inplace=True)
     return gene_quanti_table_with_gene_column_modified
 
 
