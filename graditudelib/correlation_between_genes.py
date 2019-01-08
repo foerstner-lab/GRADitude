d49cf93654ae9462a01f2ba9047aa5b0ca35506e
silvia <sylviadigiorgio@gmail.com>
2019-01-08 13:30:55 +0100

    correction in the plot

--- graditudelib/correlation_between_genes.py
+++ graditudelib/correlation_between_genes.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-plt.switch_backend('agg')
 from scipy import stats
+
+plt.switch_backend('agg')
 
 
 def correlation(
@@ -10,7 +11,7 @@ def correlation(
     feature_count_table_df_value = _extract_value_matrix(feature_count_table_df, feature_count_start_column)
     feature_count_table_df_value.set_index(['Gene'], inplace=True)
     corr_hist(feature_count_table_df_value, output_file)
-    #corr_heatmap(feature_count_table_df_value)
+    # corr_heatmap(feature_count_table_df_value)
 
 
 def _extract_value_matrix(feature_count_table_df,
@@ -20,9 +21,7 @@ def _extract_value_matrix(feature_count_table_df,
 
 def corr_hist(feature_count_table_df_value, output_file):
     rho, p = stats.spearmanr(feature_count_table_df_value.values.T)
-    rho_df = pd.DataFrame.from_records(rho)
+    rho_df = pd.DataFrame.from_records(rho, columns=feature_count_table_df_value.index,
+                                       index=feature_count_table_df_value.index)
     plt.hist(rho_df)
     plt.savefig(output_file)
-
-
-
