2d36b0bffc22fb3181e4d51586fc3928ee8e43db
silvia <sylviadigiorgio@gmail.com>
2019-01-09 14:20:02 +0100

    fixing heatmap code and documentation about it

--- graditudelib/heatmap.py
+++ graditudelib/heatmap.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 hv.extension('bokeh', width=90)
 
 
-def plot_heatmap(feature_count_table, feature_count_start_column, y_label_, output_file):
+def plot_heatmap(feature_count_table, feature_count_start_column, feature_count_end_column, y_label_, output_file):
     table = pd.read_table(feature_count_table, sep=",")
     table.set_index('Gene', inplace=True)
-    value_matrix = _extract_value_matrix(table, feature_count_start_column)
+    value_matrix = _extract_value_matrix(table, feature_count_start_column, feature_count_end_column)
     heatmaps(value_matrix, y_label_, output_file)
 
 
 def _extract_value_matrix(feature_count_table_df,
-                          feature_count_start_column):
-    return feature_count_table_df.iloc[:, int(feature_count_start_column):]
+                          feature_count_start_column, feature_count_end_column):
+    return feature_count_table_df.iloc[:, int(feature_count_start_column):feature_count_end_column]
 
 
 def heatmaps(value_matrix, y_label_, output_file):
