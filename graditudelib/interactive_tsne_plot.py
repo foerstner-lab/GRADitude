d1dae34b81a5e7c51c41dcbf01eef610ce1eb53b
silvia <sylviadigiorgio@gmail.com>
2019-01-11 11:22:28 +0100

    correct deprecation

--- graditudelib/interactive_tsne_plot.py
+++ graditudelib/interactive_tsne_plot.py
@@ -51,7 +51,7 @@ def plot_t_sne_using_clustering(read_counting_table, tsne_result, output_file_):
         color=color,
         attribute=read_counting_table["Attributes"])
 
-    source = ColumnDataSource(hower_data)
+    s1 = ColumnDataSource(hower_data)
     hover = HoverTool(tooltips=[
         ("Gene", "@gene"),
         ("Feature", "@feature"),
@@ -63,7 +63,7 @@ def plot_t_sne_using_clustering(read_counting_table, tsne_result, output_file_):
                output_backend="webgl", lod_threshold=100,
                title="Grad-Seq t-SNE RNA-Seq")
 
-    p.scatter("x", "y", source=source, size=5, alpha=2, color='color')
+    p.scatter("x", "y", source=s1, size=5, alpha=2, color='color')
     s2 = ColumnDataSource(data=dict(x=[], y=[]))
     p2 = figure(plot_width=300, plot_height=300,
                 tools="", title="Zoom", logo=None, output_backend="webgl", lod_threshold=100)
@@ -73,20 +73,22 @@ def plot_t_sne_using_clustering(read_counting_table, tsne_result, output_file_):
         TableColumn(field="feature", title="Feature"),
         TableColumn(field="attribute", title="Attributes")
     ]
-    data_table = DataTable(source=source, columns=columns, width=450, height=800, fit_columns=True)
+    data_table = DataTable(source=s1, columns=columns, width=450, height=800, fit_columns=True)
     button = Button(label="Save", button_type="success")
-    source.callback = CustomJS(args=dict(s2=s2), code="""
-    var inds = cb_obj.selected['1d'].indices;
-    var d1 = cb_obj.data;
-    var d2 = s2.data;
-    d2['x'] = []
-    d2['y'] = []
-    for (i = 0; i < inds.length; i++) {
-    d2['x'].push(d1['x'][inds[i]])
-    d2['y'].push(d1['y'][inds[i]])}
-    s2.trigger('change');
-    s2.change.emit();""")
-    button.callback = CustomJS(args=dict(source_data=source), code="""
+    s1.selected.js_on_change('indices', CustomJS(args=dict(s1=s1, s2=s2), code="""
+            var inds = cb_obj.indices;
+            var d1 = s1.data;
+            var d2 = s2.data;
+            d2['x'] = []
+            d2['y'] = []
+            for (var i = 0; i < inds.length; i++) {
+                d2['x'].push(d1['x'][inds[i]])
+                d2['y'].push(d1['y'][inds[i]])
+            }
+            s2.change.emit();
+        """)
+                             )
+    button.callback = CustomJS(args=dict(source_data=s1), code="""
             var inds = source_data.selected['1d'].indices;
             var data = source_data.data;
             var out = read.counting_table.columns;
