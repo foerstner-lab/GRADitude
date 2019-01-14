8a230b3155ed66f5ef2bd55ac9dc7cd58e5ba693
silvia <sylviadigiorgio@gmail.com>
2019-01-14 13:48:22 +0100

    add parameter

--- graditudelib/selecting_ncRNAs.py
+++ graditudelib/selecting_ncRNAs.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
 
-def selecting_specific_features(normalized_table, feature_count_start_column, features, output_file):
+def selecting_specific_features(normalized_table, feature_count_start_column,
+                                feature_count_end_column, features, output_file):
     normalized_table_df = pd.read_table(normalized_table)
     features_rows = finding_nc_rnas_rows(normalized_table_df, features)
     attribute_table = _extract_attributes(features_rows, feature_count_start_column)
-    nc_rnas_table = _extract_value_specific_feature(features_rows, feature_count_start_column)
+    nc_rnas_table = _extract_value_specific_feature(features_rows, feature_count_start_column,
+                                                    feature_count_end_column)
     pd.concat([attribute_table, nc_rnas_table],
               axis=1).to_csv(output_file, sep='\t', index=None)
 
@@ -14,8 +16,8 @@ def finding_nc_rnas_rows(normalized_table, features):
     return normalized_table.loc[normalized_table['Feature'].isin(features)]
 
 
-def _extract_value_specific_feature(features_rows, feature_count_start_column):
-    return features_rows.iloc[:, feature_count_start_column:]
+def _extract_value_specific_feature(features_rows, feature_count_start_column, feature_count_end_column):
+    return features_rows.iloc[:, feature_count_start_column:feature_count_end_column]
 
 
 def _extract_attributes(features_rows,
