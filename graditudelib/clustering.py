fdc5ba7e6a77ec39dfe9bd50c8edb609d3174ae9
silvia <sylviadigiorgio@gmail.com>
2019-01-29 11:15:30 +0100

    add parameters

--- graditudelib/clustering.py
+++ graditudelib/clustering.py
@@ -5,13 +5,13 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import DBSCAN
 
 
-def clustering(feature_count_table, feature_count_start_column,
+def clustering(feature_count_table, feature_count_start_column, feature_count_end_column,
                number_of_clusters, pseudo_count, clustering_methods,
                epsilon, min_samples, scaling_method,
                output_file):
     feature_count_table_df = pd.read_table(feature_count_table)
     value_matrix = _extract_value_matrix(feature_count_table_df,
-                                         feature_count_start_column)
+                                         feature_count_start_column, feature_count_end_column)
     attribute_matrix = _extract_attributes(feature_count_table_df,
                                            feature_count_start_column)
     normalized_table = normalize_values(value_matrix, scaling_method, pseudo_count)
@@ -26,8 +26,8 @@ def clustering(feature_count_table, feature_count_start_column,
 
 
 def _extract_value_matrix(feature_count_table_df,
-                          feature_count_start_column):
-    return feature_count_table_df.iloc[:, int(feature_count_start_column):]
+                          feature_count_start_column, feature_count_end_column):
+    return feature_count_table_df.iloc[:, feature_count_start_column:feature_count_end_column]
 
 
 def _extract_attributes(feature_count_table_df,
@@ -75,13 +75,13 @@ def normalize_values(values_matrix, scaling_method, pseudo_count):
     elif scaling_method == "log10":
         normalized_values = values_matrix.applymap(
             lambda val: val + pseudo_count).applymap(np.log10)
-    elif scaling_method == "normalized_to_max":
+    elif scaling_method == "to_max":
         values_matrix = values_matrix.fillna(lambda x: 0)
         row_max_values = values_matrix.max(axis=1)
         normalized_values = values_matrix.divide(
             row_max_values, axis=0)
         normalized_values = pd.DataFrame(normalized_values).fillna(0)
-    elif scaling_method == "normalized_to_range":
+    elif scaling_method == "to_range":
         values_matrix = values_matrix.fillna(lambda x: 0)
         row_max_values = values_matrix.max(axis=1)
         row_min_values = values_matrix.min(axis=1)
