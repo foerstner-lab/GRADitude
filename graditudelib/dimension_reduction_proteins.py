a85c3e6b3bcbf9333b7909e1e93696337fca2bae
Silvia Di Giorgio <silvia.digiorgio@uni-wuerzburg.de>
2019-05-14 14:57:29 +0200

    update the figure output

--- graditudelib/dimension_reduction_proteins.py
+++ graditudelib/dimension_reduction_proteins.py
@@ -19,15 +19,15 @@ def dr_proteins(feature_count_table, feature_count_start_column,
     if dimension_reduction_algorithm == "t-SNE":
         t_sne_result = perform_t_sne(value_matrix,
                                      perplexity)
-        plot_t_sne(value_matrix, t_sne_result,
+        plot_t_sne(feature_count_table_df, t_sne_result,
                    output_colored_clusters)
     elif dimension_reduction_algorithm == "PCA":
         pca_result = perform_pca(value_matrix)
-        plot_pca(value_matrix, pca_result,
+        plot_pca(feature_count_table_df, pca_result,
                  output_colored_clusters)
     elif dimension_reduction_algorithm == "UMAP":
         umap_result = perform_umap(value_matrix, n_neighbors, min_dist)
-        plot_umap(value_matrix, umap_result,
+        plot_umap(feature_count_table_df, umap_result,
                   output_colored_clusters)
 
 
@@ -35,7 +35,7 @@ def _extract_value_matrix(feature_count_table_df,
                           feature_count_start_column,
                           feature_count_end_column):
     return feature_count_table_df.iloc[:,
-                                       feature_count_start_column:feature_count_end_column]
+           feature_count_start_column:feature_count_end_column]
 
 
 def perform_t_sne(normalized_values, perplexity):
@@ -62,18 +62,20 @@ def plot_t_sne(read_counting_table, tsne_result, output_file_colorized_by_cluste
     read_counting_table["t-SNE-component_1"] = [pos[0] for pos in tsne_result]
     read_counting_table["t-SNE-component_2"] = [pos[1] for pos in tsne_result]
 
-    color_palette = bokeh.palettes.viridis(
-        len(read_counting_table["Cluster_label"].unique()))
+    color_palette = bokeh.palettes.Colorblind[(
+        len(read_counting_table["Cluster_label"].unique()))]
     color = read_counting_table["Cluster_label"].apply(
         lambda lable: color_palette[lable])
+    label = read_counting_table.apply(
+        _label_clustering, axis=1)
 
     hower_data = dict(
         x=read_counting_table["t-SNE-component_1"],
         y=read_counting_table["t-SNE-component_2"],
-        feature=read_counting_table["Protein.names"],
-        id=read_counting_table["Protein.IDs"],
+        feature=read_counting_table["Gene"],
         cluster_label=read_counting_table["Cluster_label"],
-        color=color)
+        color=color,
+        label=label)
 
     source = ColumnDataSource(hower_data)
 
@@ -81,14 +83,18 @@ def plot_t_sne(read_counting_table, tsne_result, output_file_colorized_by_cluste
         ("Protein.names", "@feature"),
         ("Protein.IDs", "@id")])
 
-    plot = figure(plot_width=700, plot_height=700,
+    plot = figure(plot_width=900, plot_height=900,
                   tools=[hover, BoxZoomTool(), ResetTool(), PanTool(),
                          WheelZoomTool(), "tap"],
-                  title="Grad-Seq t-SNE RNA-Seq", logo=None)
+                  title="Grad-Seq PCA RNA-Seq", logo=None)
 
-    plot.circle("x", "y", source=source, size=5, alpha=0.7, color='color')
+    plot.circle("x", "y", source=source, size=7, alpha=3,
+                color='color', legend="label", line_color="black")
+    plot.yaxis.axis_label_text_font_size = "15pt"
+    plot.xaxis.axis_label_text_font_size = "15pt"
+    plot.title.text_font_size = '15pt'
 
-    url = "http://www.uniprot.org/uniprot/@id"
+    url = "https://www.uniprot.org/uniprot/?query=@feature"
     taptool = plot.select(type=TapTool)
     taptool.callback = OpenURL(url=url)
 
@@ -103,10 +109,12 @@ def plot_pca(read_counting_table, pca_result, output_file_colorized_by_clusters)
     read_counting_table["PCA-component_1"] = [pos[0] for pos in pca_result]
     read_counting_table["PCA-component_2"] = [pos[1] for pos in pca_result]
 
-    color_palette = bokeh.palettes.viridis(
-        len(read_counting_table["Cluster_label"].unique()))
+    color_palette = bokeh.palettes.Colorblind[(
+        len(read_counting_table["Cluster_label"].unique()))]
     color = read_counting_table["Cluster_label"].apply(
         lambda lable: color_palette[lable])
+    label = read_counting_table.apply(
+        _label_clustering, axis=1)
 
     hower_data = dict(
         x=read_counting_table["PCA-component_1"],
@@ -114,7 +122,8 @@ def plot_pca(read_counting_table, pca_result, output_file_colorized_by_clusters)
         feature=read_counting_table["Protein.names"],
         id=read_counting_table["Protein.IDs"],
         cluster_label=read_counting_table["Cluster_label"],
-        color=color)
+        color=color,
+        label=label)
 
     source = ColumnDataSource(hower_data)
 
@@ -122,14 +131,18 @@ def plot_pca(read_counting_table, pca_result, output_file_colorized_by_clusters)
         ("Protein.names", "@feature"),
         ("Protein.IDs", "@id")])
 
-    plot = figure(plot_width=700, plot_height=700,
+    plot = figure(plot_width=900, plot_height=900,
                   tools=[hover, BoxZoomTool(), ResetTool(), PanTool(),
                          WheelZoomTool(), "tap"],
                   title="Grad-Seq PCA RNA-Seq", logo=None)
 
-    plot.circle("x", "y", source=source, size=5, alpha=0.7, color='color')
+    plot.circle("x", "y", source=source, size=7, alpha=3,
+                color='color', legend="label", line_color="black")
+    plot.yaxis.axis_label_text_font_size = "15pt"
+    plot.xaxis.axis_label_text_font_size = "15pt"
+    plot.title.text_font_size = '15pt'
 
-    url = "http://www.uniprot.org/uniprot/@id"
+    url = "https://www.uniprot.org/uniprot/?query=@feature"
     taptool = plot.select(type=TapTool)
     taptool.callback = OpenURL(url=url)
 
@@ -144,10 +157,12 @@ def plot_umap(read_counting_table, umap_result, output_file_colorized_by_cluster
     read_counting_table["UMAP-component_1"] = [pos[0] for pos in umap_result]
     read_counting_table["UMAP-component_2"] = [pos[1] for pos in umap_result]
 
-    color_palette = bokeh.palettes.viridis(
-        len(read_counting_table["Cluster_label"].unique()))
+    color_palette = bokeh.palettes.Colorblind[(
+        len(read_counting_table["Cluster_label"].unique()))]
     color = read_counting_table["Cluster_label"].apply(
         lambda lable: color_palette[lable])
+    label = read_counting_table.apply(
+        _label_clustering, axis=1)
 
     hower_data = dict(
         x=read_counting_table["UMAP-component_1"],
@@ -155,7 +170,8 @@ def plot_umap(read_counting_table, umap_result, output_file_colorized_by_cluster
         feature=read_counting_table["Protein.names"],
         id=read_counting_table["Protein.IDs"],
         cluster_label=read_counting_table["Cluster_label"],
-        color=color)
+        color=color,
+        label=label)
 
     source = ColumnDataSource(hower_data)
 
@@ -163,14 +179,17 @@ def plot_umap(read_counting_table, umap_result, output_file_colorized_by_cluster
         ("Protein.names", "@feature"),
         ("Protein.IDs", "@id")])
 
-    plot = figure(plot_width=700, plot_height=700,
+    plot = figure(plot_width=900, plot_height=900,
                   tools=[hover, BoxZoomTool(), ResetTool(), PanTool(),
                          WheelZoomTool(), "tap"],
-                  title="Grad-Seq t-SNE UMAP-Seq", logo=None)
+                  title="Grad-Seq PCA RNA-Seq", logo=None)
 
-    plot.circle("x", "y", source=source, size=5, alpha=0.7, color='color')
-
-    url = "http://www.uniprot.org/uniprot/@id"
+    plot.circle("x", "y", source=source, size=7, alpha=3,
+                color='color', legend="label", line_color="black")
+    plot.yaxis.axis_label_text_font_size = "15pt"
+    plot.xaxis.axis_label_text_font_size = "15pt"
+    plot.title.text_font_size = '15pt'
+    url = "https://www.uniprot.org/uniprot/?query=@feature"
     taptool = plot.select(type=TapTool)
     taptool.callback = OpenURL(url=url)
 
@@ -179,3 +198,10 @@ def plot_umap(read_counting_table, umap_result, output_file_colorized_by_cluster
 
     output_file(output_file_colorized_by_clusters)
     save(plot)
+
+
+def _label_clustering(row):
+    __label = {0: "cluster1", 1: "cluster2", 2: "cluster3",
+               3: "cluster4", 4: "cluster5", 5: "cluster6",
+               6: "cluster7", 7: "cluster8"}[row["Cluster_label"]]
+    return __label
