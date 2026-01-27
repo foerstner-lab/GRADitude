f14f2df202b0d0028346f31b35671b6dd49bb164
Silvia Di Giorgio <digiorgio@zbmed.de>
2026-01-27 15:57:34 +0100

    fixing incompatibility

--- graditudelib/merge_exctract_attributes.py
+++ graditudelib/merge_exctract_attributes.py
@@ -4,8 +4,22 @@ import numpy as np
 
 def merge_exctracted_attributes(feature_count_table, output_table):
     table = pd.read_csv(feature_count_table, sep='\t')
-    table['Parent'] = np.where(table['Parent'].isnull(), table['name'], table['Parent'])
-    table['Parent'] = np.where(table['Parent'].isnull(), table['ID'], table['Parent'])
-    table.rename(columns={'Parent': 'Gene'}, inplace=True)
-    table.to_csv(output_table, index=None, sep='\t')
 
+    # Prefer gene identifiers in this order (works with tutorial_data/gene_wise_extended.csv):
+    # gene -> Name -> Parent -> name -> ID
+    candidates = ["gene", "Name", "Parent", "name", "ID"]
+    present = [c for c in candidates if c in table.columns]
+
+    if not present:
+        raise ValueError(
+            "Cannot create 'Gene' column. None of these columns were found: "
+            f"{', '.join(candidates)}"
+        )
+
+    # Start with the first available column and fill missing values from the next ones
+    gene_series = table[present[0]]
+    for col in present[1:]:
+        gene_series = np.where(pd.isna(gene_series), table[col], gene_series)
+
+    table["Gene"] = gene_series
+    table.to_csv(output_table, index=None, sep='\t')
