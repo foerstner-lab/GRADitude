c9566df7d4132a72d28fef14332415f71b1cf528
Silvia Di Giorgio <digiorgio@zbmed.de>
2026-01-27 17:02:08 +0100

    adding compatibility with the new reademption version

--- graditudelib/min_row_sum_ercc_table.py
+++ graditudelib/min_row_sum_ercc_table.py
@@ -13,14 +13,14 @@ def filtering_ercc_table(ref_feature_count_table, min_row_sum,
 
 def _extract_gene_matrix(feature_count_table_df):
     gene_column = feature_count_table_df[list(filter(
-        lambda col: col.startswith("Libraries"), feature_count_table_df.columns))]
+        lambda col: col.startswith("Statistic"), feature_count_table_df.columns))]
     return gene_column
 
 
 def create_a_new_table_filtered_ercc(alignment_table):
     series = []
     for index, row in alignment_table.iterrows():
-        selection = row['Libraries']
+        selection = row['Statistic']
         if selection.startswith('ERCC') & ('No. of aligned reads' in selection):
             series.append(row)
     filtered_table = pd.DataFrame(series)
@@ -29,14 +29,15 @@ def create_a_new_table_filtered_ercc(alignment_table):
 
 def min_row_sum_ercc_table(table_filtered_ercc, colum_with_libraries, min_row_sum):
     gene_table_final = []
-    summed_values = table_filtered_ercc.sum(axis=1)
+    grad_cols = [col for col in table_filtered_ercc.columns if col.startswith('Grad')]
+    summed_values = table_filtered_ercc[grad_cols].sum(axis=1)
     combined_df = pd.concat([colum_with_libraries, summed_values], axis=1)
     combined_df.columns = ['Gene', 'sum_of_values']
     selected_df = combined_df[~(combined_df['sum_of_values'] <= min_row_sum)]
     selected_df.reset_index(drop=True, inplace=True)
     my_keys = selected_df['Gene'].tolist()
     for index, row in table_filtered_ercc.iterrows():
-        gene = row["Libraries"]
+        gene = row["Statistic"]
         if gene in my_keys:
             gene_table_final.append(row)
     df_with_min_row_samples = pd.DataFrame(gene_table_final)
