dea707687687b6c48cce923ff4b969bb5dea80c7
silviadigiorgio <digiorgio@zbmed.de>
2026-05-31 22:45:25 +0200

    add histograms of fractions

--- graditudelib/histograms_of_fractions.py
+++ graditudelib/histograms_of_fractions.py
@@ -3,23 +3,43 @@ import pandas as pd
 
 
 def plot_histograms(feature_count_table,
-                    feature_count_start_column):
+                    feature_count_start_column,
+                    feature_count_end_column):
+
     feature_count_table_df = pd.read_table(feature_count_table)
-    feature_count_table_df_value = _extract_value_matrix(feature_count_table_df,
-                                                         feature_count_start_column)
+
+    feature_count_table_df_value = _extract_value_matrix(
+        feature_count_table_df,
+        feature_count_start_column,
+        feature_count_end_column
+    )
+
     _plot_histogram_pdf(feature_count_table_df_value)
 
 
 def _extract_value_matrix(feature_count_table_df,
-                          feature_count_start_column):
-    return feature_count_table_df.iloc[:, int(feature_count_start_column):]
+                          feature_count_start_column,
+                          feature_count_end_column):
+
+    return feature_count_table_df.iloc[
+        :,
+        int(feature_count_start_column):
+        int(feature_count_end_column) + 1
+    ]
 
 
 def _plot_histogram_pdf(feature_count_table_df_value):
-    i = 0
+
     for column in feature_count_table_df_value:
-        i = i + 1
-        plt.figure(i)
-        plt.hist(feature_count_table_df_value[column].values)
-        # plt.show()
-        plt.savefig(column)
+
+        plt.figure()
+
+        plt.hist(
+            feature_count_table_df_value[column].values
+        )
+
+        plt.title(column)
+
+        plt.savefig(f"{column}.pdf")
+
+        plt.close()
